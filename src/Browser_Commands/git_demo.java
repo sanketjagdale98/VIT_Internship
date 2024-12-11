a9aefd950874171f9a707e400afdd879cd6e204c
sanket <sanketjagdale98@gmail.com>
2024-12-11 16:04:46 +0530

    sample demo file

---
+++ src/Browser_Commands/git_demo.java
@@ -0,0 +1,30 @@
+package Browser_Commands;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class git_demo {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.manage().window().maximize();
+		
+		String  pageTitle = driver.getTitle();
+		System.out.println("Facebook Login Page Title :" + pageTitle);
+		
+		String currentURL = driver.getCurrentUrl();
+		System.out.println("Facebook current URL :" + currentURL);
+		
+		String pageSource = driver.getPageSource();
+		System.out.println(pageSource.contains(currentURL));
+		
+		driver.close();
+		
+		
+	
+
+	}
+
+}
